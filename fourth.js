656ff49839caa339599bfd02234c688103e753d5
ArjunDwivedi <148845104+ArjunDwivedi@users.noreply.github.com>
2024-04-21 14:50:03 +0530

    Add files via upload

---
+++ fourth.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req,res)=>{
+    const url= req.url;
+    const method = req.method;
+    const body = [];
+    
+    if(url==='/'){
+        fs.readFile('message.txt',{encoding:"utf-8"},(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+            res.setHeader("content-type","text/html");
+            res.write("<html>");
+            res.write("<head><title>Enter Message</title></head>");
+            res.write(`<body>${data}</body>`);
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+            res.write("</html>");
+            return res.end();
+            }
+        });
+    }
+    else if(url === '/message' && method === "POST"){
+        req.on("data",(chunk)=>{
+            body.push(chunk);
+        });
+    return req.on("end",()=>{
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split("=")[1];        
+        fs.writeFile("message.txt",message,(err)=>{
+            if(err){
+                console.log(err)
+            }
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
+    });
+    }
+    else{
+        res.setHeader("content-type","text/html");
+        res.write('<html>')
+        res.write('<head><title>home page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end()
+    }
+});
+
+server.listen(3000);
